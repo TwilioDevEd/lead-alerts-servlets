f6472a25571a3475d045d1a1692dcef23fe7c9a2
mosampaio <mosampaio@gmail.com>
2016-09-14 11:06:14 -0300

    Bumps twilio sdk version to 7.0.0-rc-25

--- src/main/java/com/twilio/leadalerts/lib/MessageSender.java
+++ src/main/java/com/twilio/leadalerts/lib/MessageSender.java
@@ -1,42 +1,31 @@
 package com.twilio.leadalerts.lib;
 
+import com.twilio.http.TwilioRestClient;
 import com.twilio.leadalerts.lib.config.Credentials;
 import com.twilio.leadalerts.lib.config.PhoneNumbers;
-import com.twilio.sdk.TwilioRestClient;
-import com.twilio.sdk.TwilioRestException;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.twilio.rest.api.v2010.account.MessageCreator;
+import com.twilio.type.PhoneNumber;
 
 @SuppressWarnings("unused")
 public class MessageSender {
     private final TwilioRestClient client;
 
     public MessageSender() {
-        client = new TwilioRestClient(
+        client = new TwilioRestClient.Builder(
                 Credentials.getTwilioAccountSid(),
-                Credentials.getTwilioAuthToken());
+                Credentials.getTwilioAuthToken()).build();
     }
 
     public MessageSender(TwilioRestClient client) {
         this.client = client;
     }
 
-    public void send(String message) throws TwilioRestException {
-        List<NameValuePair> params = buildParams(message);
-
-        client.getAccount().getMessageFactory().create(params);
+    public void send(String message) {
+        new MessageCreator(
+                new PhoneNumber(PhoneNumbers.getAgentPhoneNumber()),
+                new PhoneNumber(PhoneNumbers.getTwilioPhoneNumber()),
+                message
+        ).execute(client);
     }
 
-    private List<NameValuePair> buildParams(String message) {
-        List<NameValuePair> params = new ArrayList<>();
-
-        params.add(new BasicNameValuePair("To", PhoneNumbers.getAgentPhoneNumber()));
-        params.add(new BasicNameValuePair("From", PhoneNumbers.getTwilioPhoneNumber()));
-        params.add(new BasicNameValuePair("Body", message));
-
-        return params;
-    }
 }
